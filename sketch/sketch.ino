e7fa9abf54c3077344779c6a3f52c2a674a1a3bc
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-03-18 10:07:38 +0100

    Add Arduino RouterBridge functionality and monitor output

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,1 +1,32 @@
+#include <Arduino.h>
+#include <Arduino_RouterBridge.h>
 
+void printSlow(const char *s) {
+  for (int i = 0; s[i] != '\0'; i++) {
+    Monitor.print(s[i]);
+    delay(20);  // volontairement long
+  }
+  Monitor.println();
+}
+
+void callbackTest(int x) {
+  Monitor.println(">>> CALLBACK EXECUTED <<<");
+}
+
+void setup() {
+  Bridge.begin();
+  Monitor.begin();
+
+  Bridge.provide_safe("test", callbackTest);
+  //Bridge.provide("test", callbackTest);
+}
+
+void loop() {
+  Monitor.println("[LOOP START]");
+
+  printSlow("Forum");
+
+  delay(100);
+
+  Monitor.println("[LOOP END]");
+}
